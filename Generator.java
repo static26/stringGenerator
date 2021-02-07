93b343a97b28b311a75fb0a5c222dfc5ce301021
static <74821435+static26@users.noreply.github.com>
2021-02-07 17:19:30 +0200

    Add files via upload

---
+++ Generator.java
@@ -0,0 +1,24 @@
+package com.example.passwordgenerator;
+
+import java.util.Random;
+
+public class Generator {
+
+
+    public static String alfabeLower = "abcdefghytzxvbnmlşipou";
+    public static String alfabeUpper = alfabeLower.toUpperCase().replace("İ", "I");
+    public static String numbers = "0123456789";
+    public static String specielsChars = ".!?*%";
+
+    public static String allStr = alfabeLower + alfabeUpper + numbers + specielsChars;
+
+    public static String generateRandomString(int size){
+        String text = "";
+        char[] chars = allStr.toCharArray();
+        for(int j = 0; j < size; j++){
+            char c = chars[new Random().nextInt(chars.length)];
+            text += c;
+        }
+        return text;
+    }
+}
